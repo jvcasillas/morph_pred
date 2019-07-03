8059fe2099d95bd41c9dd46e50889f1951a54c71
jvcasillas <joseph.casillas@rutgers.edu>
2019-07-03 11:23:36 -0400

    fitted some models

--- scripts/stress/s1_beg_adv_nat/eye_track/01_bmr_landmarks.R
+++ scripts/stress/s1_beg_adv_nat/eye_track/01_bmr_landmarks.R
@@ -44,7 +44,7 @@ stress_df <- stress_lm %>%
 
 
 
-# BRM at target word onset ----------------------------------------------------
+# Model setup -----------------------------------------------------------------
 
 # Detect cores for model fitting
 options(mc.cores = parallel::detectCores())
@@ -54,36 +54,35 @@ priors <- c(
   set_prior("normal(0, 10)", class = "b")
 )
 
-mod_tw_start_grp <- brm(
-  formula = targetCount | trials(n_rep) ~ 0 + group + stress + syllable +
+# Model formula
+full_model <- bf(
+  targetCount | trials(n_rep) ~ group + stress + syllable +
+    phon_prob_std + biphon_prob_std +
     group:stress + group:syllable + stress:syllable + group:stress:syllable +
     (1 + stress * syllable | participant) +
-    (1 | target),
+    (1 + group | target)
+  )
+
+# -----------------------------------------------------------------------------
+
+
+
+
+
+
+
+# BRM at target word onset ----------------------------------------------------
+
+mod_tw_start_full <- brm(
+  formula = full_model,
   prior = priors,
   warmup = 1000, iter = 2000, chains = 4,
   family = binomial(link = "logit"),
   cores = 16,
   data = filter(stress_df, landmark_2 == "tw_start"),
   file = here("models", "stress", "s1_beg_adv_nat", "eye_track",
-              "mod_tw_start_grp")
+              "mod_tw_start_full")
 )
-
-
-fixef(mod_tw_start_grp) %>% inv_logit_scaled()
-
-
-
-pp_check(mod_tw_start_grp, nsamples = 200)
-two_grp_post  <- posterior_samples(mod_tw_start_grp)
-hdi(two_grp_post)
-
-two_grp_post %>%
-  select(., b_groupss:b_grouplb) %>%
-  mutate_all(., inv_logit_scaled) %>%
-  gather(parameter, estimate) %>%
-  ggplot(., aes(x = estimate, y = parameter)) +
-    ggridges::geom_density_ridges2(color = "white", fill = "grey80") +
-    theme_minimal()
 
 # -----------------------------------------------------------------------------
 
@@ -93,41 +92,63 @@ two_grp_post %>%
 
 # BRM at 1st syllable offset --------------------------------------------------
 
-mod_ts_end_null <- brm(
-  formula = targetCount | trials(n_rep) ~ 1 +
-    (1 | participant) +
-    (1 | target),
-  prior = priors,
-  warmup = 1000, iter = 2000, chains = 4,
-  family = binomial(link = "logit"),
-  cores = 16,
-  data = filter(stress_df, landmark_2 == "tw_syl1_end"),
-  file = here("models", "stress", "s1_beg_adv_nat", "eye_track",
-              "mod_ts_end_null")
-)
-
-mod_ts_end_grp <- update(
-  object = mod_ts_end_null,
-  formula = targetCount | trials(n_rep) ~ 1 + group +
-    (1 | participant) +
-    (1 | target),
-  newdata = filter(stress_df, landmark_2 == "tw_syl1_end"),
-  file = here("models", "stress", "s1_beg_adv_nat", "eye_track",
-              "mod_ts_end_grp")
-)
-
 mod_ts_end_full <- update(
-  object = mod_ts_end_grp,
-  formula = targetCount | trials(n_rep) ~ 1 +
-    group + stress + syllable + phon_prob_std + biphon_prob_std +
-    group:stress + group:syllable + group:stress:syllable +
-    (1 + stress + syllable | participant) +
-    (1 | target),
+  object = mod_tw_start_full,
   newdata = filter(stress_df, landmark_2 == "tw_syl1_end"),
   file = here("models", "stress", "s1_beg_adv_nat", "eye_track",
               "mod_ts_end_full")
 )
 
-pp_check(mod_tso_full, nsamples = 200)
+# -----------------------------------------------------------------------------
 
-plot(mod_tso_full)
+
+
+
+
+
+
+# Diagnostics -----------------------------------------------------------------
+
+
+fixef(mod_tw_start_full) %>% inv_logit_scaled()
+
+pp_check(mod_tw_start_full, nsamples = 200)
+two_full_post  <- posterior_samples(mod_tw_start_full)
+tse_full_post  <- posterior_samples(mod_ts_end_full)
+
+hdi(two_full_post)
+
+two_full_post %>%
+  select(., b_Intercept:b_grouplb) %>%
+  mutate_all(., inv_logit_scaled) %>%
+  gather(parameter, estimate) %>%
+  ggplot(., aes(x = estimate, y = parameter)) +
+    ggridges::geom_density_ridges2(color = "white", fill = "grey80") +
+    theme_minimal()
+
+tse_full_post %>%
+  select(., b_Intercept:b_grouplb) %>%
+  mutate_all(., inv_logit_scaled) %>%
+  gather(parameter, estimate) %>%
+  ggplot(., aes(x = estimate, y = parameter)) +
+    ggridges::geom_density_ridges2(color = "white", fill = "grey80") +
+    theme_minimal()
+
+
+# -----------------------------------------------------------------------------
+
+
+full_posterior <- bind_rows(
+  posterior_samples(mod_tw_start_full) %>%
+    mutate(landmark = "tw_start"),
+  posterior_samples(mod_ts_end_full) %>%
+    mutate(landmark = "ts_end"))
+
+full_posterior %>%
+  select(., b_Intercept:b_grouplb, landmark) %>%
+  mutate_if(., is.numeric, inv_logit_scaled) %>%
+  gather(parameter, estimate, -landmark) %>%
+  ggplot(., aes(x = estimate, y = parameter)) +
+    facet_grid(. ~ landmark) +
+    ggridges::geom_density_ridges2(color = "white", fill = "grey80") +
+    theme_minimal()
